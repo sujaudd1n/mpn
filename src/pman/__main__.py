a6b798a0fbe6e0e8f0027bd8e718c0c76d14e23c
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2024-09-23 23:23:11 +0530

    Modify Python files

--- src/pman/__main__.py
+++ src/pman/__main__.py
@@ -2,5 +2,4 @@ import sys
 from pman.python_help import show_python_help
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    sys.exit(show_python_help(args))
+    sys.exit(show_python_help())
--- src/pman/python_help.py
+++ src/pman/python_help.py
@@ -1,3 +1,6 @@
-def show_python_help(objs):
+import sys
+
+def show_python_help():
+    objs = sys.argv[1:]
     for obj in objs:
         help(obj)
